945fe4797e69198f7b9b808f58ca54bc7696e58b
Алексей <@DESKTOP-S2DFK6G>
2021-11-06 12:37:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleGame_1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ConsoleGame_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Random rnd = new Random();
+
+            Robot user = new Robot(2, 12, 3, 5);
+            Robot pc = new Robot(1, 13, 3, 0);
+
+            Console.WriteLine("Давайте сыграем! Ваша задача выжить и убить противника.");
+            Console.WriteLine("Показатель здоровья вашего робота: {0}, количество патронов: {1}", user.hp, user.bullets);
+            Console.WriteLine("Показатель здоровья робота противника: {0}, количество патронов: {1}", pc.hp, pc.bullets);
+            Console.WriteLine("____________Начинаем!____________");
+
+
+            while (user.hp > 0 & pc.hp > 0)
+            {
+                Console.Write("Что будете делать? \n 1. Выстрел \n 2. Исцеление \n 3. Купить патроны \n");
+
+                string move = Console.ReadLine();
+
+                switch (move)
+                {
+                    case "1":
+                        user.Shot(pc);
+                        break;
+                    case "2":
+                        user.Heal();
+                        break;
+                    case "3":
+                        user.Purchase();
+                        break;
+                    default:
+                        Console.WriteLine("Команда введена неверно! Попробуйте еще раз.");
+                        continue;
+
+                }
+
+                if (pc.hp < 0) break;
+
+                int move_1 = rnd.Next(1, 4);
+
+                switch (move_1)
+                {
+                    case 1:
+                        if (pc.hp < pc.max_hp)
+                        {
+                            pc.Heal();
+                            Console.WriteLine("Противник лечится.");
+                        }
+                        else continue;
+                        break;
+
+                    case 2:
+                        if (pc.bullets > 0)
+                        {
+                            pc.Shot(user);
+                            Console.WriteLine("Противник стреляет!!!");
+                        }
+                        else
+                        {
+                            pc.Purchase();
+                            Console.WriteLine("Противник покупает патроны.");
+                        }
+                        break;
+
+                    case 3:
+                        pc.Purchase();
+                        Console.WriteLine("Противник покупает патроны.");
+                        break;
+
+                }
+
+                Console.WriteLine("___________________________");
+                Console.WriteLine("| Ваши  показатели |   Очки здоровья: {0}    Патроны: {1}", user.hp, user.bullets);
+                Console.WriteLine("| Показатели врага |   Очки здоровья: {0}    Патроны: {1} \n", pc.hp, pc.bullets);
+
+
+            }
+
+
+            if (user.hp <= 0) Console.WriteLine("Поражение! Противник оказался сильнее...");
+            else if (pc.hp <= 0) Console.WriteLine("Вы устранили противника! Победа!");
+
+        }
+    }
+}
---
+++ ConsoleGame_1/Robot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame_1
+{
+    class Robot
+    {
+        public int armor, hp, damage, bullets, max_hp;
+        public Robot(int a, int b, int c, int d) { armor = a; hp = b; damage = c; bullets = d; max_hp = b; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Броня: {armor}  Жизнь: {hp}  Урон: {damage} Патроны: {bullets}");
+        }
+
+        public void Shot(Robot def)
+        {
+
+            if (bullets > 0)
+            {
+                def.hp = def.hp - damage + def.armor;
+                bullets = bullets - 1;
+
+            }
+
+            else Console.WriteLine("Недостаточно патронов!");
+
+        }
+
+        public void Heal()
+        {
+            int n = 2;
+
+            if (hp < max_hp)
+            {
+                hp = hp + n;
+            }
+
+            else
+            {
+                Console.WriteLine("У вас достаточно здоровья!");
+
+            }
+        }
+
+        public void Purchase()
+        {
+            int m = 1;
+            bullets = bullets + m;
+        }
+    }
+}
